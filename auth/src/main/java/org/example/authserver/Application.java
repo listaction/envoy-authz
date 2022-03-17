bd6b49f172e0e769e0e4e74d08c901063c35cc1a
Valeriy Navozenko <valeriy.navozenko@lendistry.com>
2022-03-18 00:16:14 +0100

    SBLAPP-477 token invalidation upon signout

--- auth/src/main/java/org/example/authserver/Application.java
+++ auth/src/main/java/org/example/authserver/Application.java
@@ -4,15 +4,19 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.example.authserver.config.AppProperties;
+import org.example.authserver.service.RedisService;
 import org.example.authserver.service.zanzibar.AclFilterService;
 import org.example.authserver.service.AuthService;
 import org.example.authserver.service.CacheLoaderService;
+import org.example.authserver.service.zanzibar.TokenService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import redis.clients.jedis.JedisPool;
 
+import javax.annotation.Nullable;
 import javax.annotation.PostConstruct;
 
 @Slf4j
@@ -22,12 +26,16 @@ import javax.annotation.PostConstruct;
 public class Application {
 
     private final AclFilterService aclFilterService;
+    private final RedisService redisService;
     private final CacheLoaderService cacheLoaderService;
     private final AppProperties appProperties;
     private final int grpcPort;
+    private final TokenService tokenService;
 
-    public Application(AclFilterService aclFilterService, CacheLoaderService cacheLoaderService, AppProperties appProperties, @Value("${grpc.port:8080}") int grpcPort) {
+    public Application(@Nullable JedisPool jedisPool, AclFilterService aclFilterService, CacheLoaderService cacheLoaderService, AppProperties appProperties, @Value("${grpc.port:8080}") int grpcPort, TokenService tokenService) {
         this.aclFilterService = aclFilterService;
+        this.redisService = new RedisService(jedisPool);
+        this.tokenService = tokenService;
         this.cacheLoaderService = cacheLoaderService;
         this.appProperties = appProperties;
         this.grpcPort = grpcPort;
@@ -38,7 +46,7 @@ public class Application {
         cacheLoaderService.subscribe();
 
         Server server = ServerBuilder.forPort(grpcPort)
-                .addService(new AuthService(aclFilterService, appProperties))
+                .addService(new AuthService(aclFilterService, appProperties, redisService, tokenService))
                 .build();
 
         server.start();
--- auth/src/main/java/org/example/authserver/service/AuthService.java
+++ auth/src/main/java/org/example/authserver/service/AuthService.java
@@ -3,28 +3,41 @@ package org.example.authserver.service;
 import com.google.rpc.Status;
 import io.envoyproxy.envoy.config.core.v3.HeaderValue;
 import io.envoyproxy.envoy.config.core.v3.HeaderValueOption;
-import io.envoyproxy.envoy.service.auth.v3.AuthorizationGrpc;
-import io.envoyproxy.envoy.service.auth.v3.CheckRequest;
-import io.envoyproxy.envoy.service.auth.v3.CheckResponse;
-import io.envoyproxy.envoy.service.auth.v3.OkHttpResponse;
+import io.envoyproxy.envoy.service.auth.v3.*;
+import io.envoyproxy.envoy.type.v3.HttpStatus;
+import io.envoyproxy.envoy.type.v3.StatusCode;
 import io.grpc.stub.StreamObserver;
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.example.authserver.config.AppProperties;
 import org.example.authserver.entity.CheckResult;
 import org.example.authserver.service.zanzibar.AclFilterService;
+import org.example.authserver.service.zanzibar.TokenService;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class AuthService extends AuthorizationGrpc.AuthorizationImplBase {
 
     private static final Integer OK = 0;
-    private static final Integer PERMISSION_DENIED=7;
+    private static final Integer PERMISSION_DENIED = 7;
+    private static final Integer UNAUTHORIZED = 16;
 
     private final AclFilterService aclFilterService;
     private final AppProperties appProperties;
+    private final RedisService redisService;
+    private final TokenService tokenService;
 
-    public AuthService(AclFilterService aclFilterService, AppProperties appProperties) {
+    private final static Pattern pattern = Pattern.compile("(.*)\\/realms\\/(.*)");
+
+    private static final String SIGNOUT_REDIS_KEY = "%s__%s";
+
+    public AuthService(AclFilterService aclFilterService, AppProperties appProperties, RedisService redisService, TokenService tokenService) {
         this.aclFilterService = aclFilterService;
         this.appProperties = appProperties;
+        this.redisService = redisService;
+        this.tokenService = tokenService;
     }
 
     @Override
@@ -33,6 +46,15 @@ public class AuthService extends AuthorizationGrpc.AuthorizationImplBase {
                 request.getAttributes().getRequest().getHttp().getMethod(),
                 request.getAttributes().getRequest().getHttp().getPath()
         );
+
+        CheckResponse unauthorizedCheckResult = validateTokenWithSignOutRequest(request);
+        if (unauthorizedCheckResult != null) {
+            responseObserver.onNext(unauthorizedCheckResult);
+            responseObserver.onCompleted();
+
+            return;
+        }
+
         CheckResult result = aclFilterService.checkRequest(request);
 
         HeaderValue headerAllowedTags = HeaderValue.newBuilder()
@@ -52,7 +74,7 @@ public class AuthService extends AuthorizationGrpc.AuthorizationImplBase {
         if (result.isMappingsPresent()) {
             log.info("request allowed: {}", result.isResult());
 
-            if (!result.isResult()){
+            if (!result.isResult()) {
                 log.trace("REJECTED by mapping id: {}", result.getRejectedWithMappingId());
             }
 
@@ -69,6 +91,47 @@ public class AuthService extends AuthorizationGrpc.AuthorizationImplBase {
 
     private int getCode(boolean allow) {
         return allow ? OK : PERMISSION_DENIED;
+    }
+
+    private CheckResponse validateTokenWithSignOutRequest(CheckRequest request) {
+        try {
+            Claims claims = tokenService.getAllClaimsFromRequest(request);
+
+            String tenant = getTenant(claims);
+            if (tenant != null) {
+                String key = String.format(SIGNOUT_REDIS_KEY, tenant, claims.get("jti").toString());
+                String expirationTimeValue = redisService.get(key);
+
+                if (expirationTimeValue != null) {
+                    // expirationTimeValue is in seconds
+                    long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+                    if (Long.parseLong(expirationTimeValue) >= currentTimeInSeconds) {
+                        return CheckResponse.newBuilder()
+                                .setStatus(Status.newBuilder().setCode(UNAUTHORIZED))
+                                .setDeniedResponse(
+                                        DeniedHttpResponse.newBuilder()
+                                                .setStatus(HttpStatus.newBuilder()
+                                                        .setCode(StatusCode.Unauthorized)
+                                                        .build())
+                                                .build())
+                                .build();
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            log.warn("Redis service is unavailable");
+        }
+
+        return null;
+    }
+
+    private String getTenant(Claims claims) {
+        Matcher matcher = pattern.matcher(claims.getIssuer());
+        if (matcher.matches() && matcher.groupCount() >= 2) {
+            return matcher.group(2);
+        }
+
+        return null;
     }
 
 }
