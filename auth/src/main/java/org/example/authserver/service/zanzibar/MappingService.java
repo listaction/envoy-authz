4fb9910f669beb5b189cad199ebe0b6e597d049f
Alex Kuleshov <alex@kuleshov.tech>
2022-03-22 13:03:38 -0400

    formatting

--- auth/src/main/java/org/example/authserver/service/zanzibar/MappingService.java
+++ auth/src/main/java/org/example/authserver/service/zanzibar/MappingService.java
@@ -40,7 +40,7 @@ public class MappingService {
     /**
      * @return mapping variables, or {@code null} for no match
      */
-    public List<Mapping> processRequest(CheckRequest request, Claims claims){
+    public List<Mapping> processRequest(CheckRequest request, Claims claims) {
         Map<MappingEntity, Map<String, String>> mappings = findMappings(request);
         if (mappings == null) return null; // no match
 
@@ -55,18 +55,18 @@ public class MappingService {
             Mapping mapping = new Mapping(entry.getKey());
             mapping.getVariableMap().putAll(entry.getValue());
 
-            if (mappingEntity.getBodyMapping() != null && ("POST".equalsIgnoreCase(requestMethod) || "PUT".equals(requestMethod))) {
+            if (mappingEntity.getBodyMapping() != null && ("POST".equalsIgnoreCase(requestMethod) || "PUT".equalsIgnoreCase(requestMethod))) {
                 String requestBody = request.getAttributes().getRequest().getHttp().getBody();
                 BodyMapping bodyMapping = mappingEntity.getBodyMapping();
                 mapping.getVariableMap().putAll(parseRequestJsonBody(bodyMapping, requestBody));
             }
 
-            if (mappingEntity.getHeaderMapping() != null){
+            if (mappingEntity.getHeaderMapping() != null) {
                 mapping.getVariableMap().putAll(parseHeaders(mappingEntity.getHeaderMapping(), headersMap));
             }
 
             Matcher m = pattern.matcher(claims.getIssuer());
-            if (m.matches() && m.groupCount() >= 2){
+            if (m.matches() && m.groupCount() >= 2) {
                 mapping.getVariableMap().put("tenant", m.group(2));
             }
 
@@ -81,32 +81,32 @@ public class MappingService {
         return result;
     }
 
-    public Map<MappingEntity, Map<String, String>> findMappings(CheckRequest request){
+    public Map<MappingEntity, Map<String, String>> findMappings(CheckRequest request) {
         List<MappingEntity> mappings = mappingCacheService.getAll();
         Map<MappingEntity, Map<String, String>> result = new HashMap<>();
 
         String requestMethod = request.getAttributes().getRequest().getHttp().getMethod();
 
-        for (MappingEntity currentMapping : mappings){
+        for (MappingEntity currentMapping : mappings) {
             Map<String, String> route;
             String mappingMethod = currentMapping.getMethod().name();
-            if (!requestMethod.equalsIgnoreCase(mappingMethod)){
+            if (!requestMethod.equalsIgnoreCase(mappingMethod)) {
                 continue; // skip entries that don't match
             }
 
             String path = removeQuery(request.getAttributes().getRequest().getHttp().getPath());
             PathPatternParser parser = new PathPatternParser();
             PathPatternRouteMatcher matcher = new PathPatternRouteMatcher(parser);
-            route  = matcher.matchAndExtract(currentMapping.getPath(), matcher.parseRoute(path));
+            route = matcher.matchAndExtract(currentMapping.getPath(), matcher.parseRoute(path));
 
-            if (route == null){
+            if (route == null) {
                 continue; // skip entries that don't match
             }
 
             log.debug("route: {}", route);
 
             Map<String, String> pathVariables = new HashMap<>();
-            for (Map.Entry<String, String> entry : route.entrySet()){
+            for (Map.Entry<String, String> entry : route.entrySet()) {
                 pathVariables.put("path." + entry.getKey(), entry.getValue()); // transform path X variable to path.X
             }
 
@@ -139,7 +139,7 @@ public class MappingService {
 
     private Map<String, String> parseHeaders(List<HeaderMappingKey> headerMapping, Map<String, String> headersMap) {
         Map<String, String> variables = new HashMap<>();
-        for (HeaderMappingKey h : headerMapping){
+        for (HeaderMappingKey h : headerMapping) {
             variables.put("body." + h.getNamespace(), headersMap.get(h.getName()));
         }
 
@@ -157,7 +157,7 @@ public class MappingService {
     private Map<String, String> parseRequestJsonBody(BodyMapping bodyMapping, String requestBody) {
         Map<String, String> variables = new HashMap<>();
         DocumentContext jsonContext = JsonPath.parse(requestBody);
-        for (BodyMappingKey k : bodyMapping.getKeys()){
+        for (BodyMappingKey k : bodyMapping.getKeys()) {
             String value = jsonContext.read(k.getXpath());
             variables.put("body." + k.getNamespace(), value);
         }
