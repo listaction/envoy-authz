fe0cadf47206b5ec69cd4ad30916f0ea117254dc
Georgii Ovechkin <georgii_ovechkin@epam.com>
2022-10-25 15:59:37 +0400

    Lombok upgrade; Service refactor

--- auth/src/main/java/org/example/authserver/service/CacheService.java
+++ auth/src/main/java/org/example/authserver/service/CacheService.java
@@ -3,135 +3,128 @@ package org.example.authserver.service;
 import authserver.acl.Acl;
 import authserver.acl.AclRelationConfig;
 import io.micrometer.core.annotation.Timed;
+
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.MurmurHash3;
 import org.example.authserver.Utils;
 import org.example.authserver.entity.RelCache;
 import org.example.authserver.repo.RelCacheRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class CacheService {
+    private final MeterService meterService;
+    private final RelCacheRepository relCacheRepository;
 
-  private static final Executor executor = Executors.newFixedThreadPool(1);
-  private static final Map<String, AclRelationConfig> configs = new ConcurrentHashMap<>();
+    private static final Executor executor = Executors.newFixedThreadPool(1);
+    private static final Map<String, AclRelationConfig> configs = new ConcurrentHashMap<>();
 
-  private final MeterService meterService;
-  private final RelCacheRepository relCacheRepository;
-
-  public CacheService(MeterService meterService, RelCacheRepository relCacheRepository) {
-    this.meterService = meterService;
-    this.relCacheRepository = relCacheRepository;
-  }
-
-  public void updateConfigs(Map<String, AclRelationConfig> configMap) {
-    configs.keySet().removeIf(cfg -> !configMap.containsKey(cfg));
-    configs.putAll(configMap);
-  }
-
-  public void updateConfig(AclRelationConfig config) {
-    configs.put(config.getNamespace(), config);
-  }
-
-  public void deleteConfig(String namespace) {
-    configs.remove(namespace);
-  }
-
-  public Map<String, AclRelationConfig> getConfigs() {
-    return configs;
-  }
-
-  @Timed(
-      value = "relation.getCached",
-      percentiles = {0.99, 0.95, 0.75})
-  public Set<String> getCachedRelations(
-      String principal, String namespace, String object, String path, long maxAclUpdate) {
-    List<RelCache> cache =
-        relCacheRepository.findAllByUsrAndRevAndNsobjectAndPath(
-            principal, maxAclUpdate, Utils.createNsObject(namespace, object), path);
-    if (cache == null || cache.size() == 0) return new HashSet<>();
-    meterService.countHitsCache();
-    return cache.stream()
-        .map(
-            m -> {
-              Set<String> tags = new HashSet<>();
-              tags.add(Utils.createTag(m.getNsobject(), m.getRelation()));
-
-              if (m.getNestedRelations() != null) {
-                tags.addAll(m.getNestedRelations());
-              }
-
-              return tags;
-            })
-        .flatMap(Collection::stream)
-        .collect(Collectors.toSet());
-  }
-
-  public void purgeCacheAsync(String principal, long rev) {
-    executor.execute(
-        () -> {
-          purgeCache(principal, rev);
-        });
-  }
-
-  public void purgeCache(String principal, long rev) {
-    log.info("purge cache for {}, rev < {}", principal, rev);
-    List<RelCache> recordsToDelete = relCacheRepository.findAllByUsrAndRevLessThan(principal, rev);
-    relCacheRepository.deleteAll(recordsToDelete);
-  }
-
-  public void persistCacheAsync(
-      String principal, Collection<String> relations, String path, Long rev) {
-    executor.execute(
-        () -> {
-          persistCache(principal, relations, path, rev);
-        });
-  }
-
-  public void persistCache(String principal, Collection<String> relations, String path, Long rev) {
-    List<RelCache> cache = new ArrayList<>();
-    for (String tag : relations) {
-      Set<String> nestedTags = new HashSet<>(relations);
-      nestedTags.remove(tag); // remove current
-      String compositeIdKey = getCompositeIdKey(principal, tag, path);
-      String id = getKeyHash(compositeIdKey);
-      Acl parsedTag = Utils.parseTag(tag);
-      if (parsedTag == null) continue;
-
-      RelCache relCache =
-          RelCache.builder()
-              .id(id)
-              .rev(rev)
-              .nsobject(Utils.createNsObject(parsedTag.getNamespace(), parsedTag.getObject()))
-              .relation(parsedTag.getRelation())
-              .nestedRelations(nestedTags)
-              .usr(principal)
-              .path(path)
-              .build();
-
-      cache.add(relCache);
+    public void updateConfigs(Map<String, AclRelationConfig> configMap) {
+        configs.keySet().removeIf(cfg -> !configMap.containsKey(cfg));
+        configs.putAll(configMap);
     }
 
-    relCacheRepository.saveAll(cache);
-  }
-
-  private String getKeyHash(String compositeIdKey) {
-    long[] vec = MurmurHash3.hash128(compositeIdKey.getBytes(StandardCharsets.UTF_8));
-    StringBuilder sb = new StringBuilder();
-    for (long l : vec) {
-      sb.append(Long.toHexString(l));
+    public void updateConfig(AclRelationConfig config) {
+        configs.put(config.getNamespace(), config);
     }
-    return sb.toString();
-  }
 
-  protected String getCompositeIdKey(String principal, String tag, String path) {
-    return String.format("%s+%s+%s", principal, tag, path);
-  }
+    public void deleteConfig(String namespace) {
+        configs.remove(namespace);
+    }
+
+    public Map<String, AclRelationConfig> getConfigs() {
+        return configs;
+    }
+
+    @Timed(value = "relation.getCached", percentiles = {0.99, 0.95, 0.75})
+    public Set<String> getCachedRelations(String principal, String namespace, String object, String path, long maxAclUpdate) {
+        List<RelCache> cache = relCacheRepository.findAllByUsrAndRevAndNsobjectAndPath(principal, maxAclUpdate, Utils.createNsObject(namespace, object), path);
+
+        if (CollectionUtils.isEmpty(cache)) {
+            return new HashSet<>();
+        }
+
+        meterService.countHitsCache();
+
+        return cache.stream()
+                .map(relCache -> {
+                            Set<String> tags = new HashSet<>();
+                            tags.add(Utils.createTag(relCache.getNsobject(), relCache.getRelation()));
+                            Optional.ofNullable(relCache.getNestedRelations()).ifPresent(tags::addAll);
+
+                            return tags;
+                        })
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+    }
+
+    public void purgeCacheAsync(String principal, long rev) {
+        executor.execute(() -> purgeCache(principal, rev));
+    }
+
+    public void purgeCache(String principal, long rev) {
+        log.info("purge cache for {}, rev < {}", principal, rev);
+        List<RelCache> recordsToDelete = relCacheRepository.findAllByUsrAndRevLessThan(principal, rev);
+        relCacheRepository.deleteAll(recordsToDelete);
+    }
+
+    public void persistCacheAsync(String principal, Collection<String> relations, String path, Long rev) {
+        executor.execute(() -> persistCache(principal, relations, path, rev));
+    }
+
+    public void persistCache(String principal, Collection<String> relations, String path, Long rev) {
+        List<RelCache> cache = new ArrayList<>();
+        for (String tag : relations) {
+            Set<String> nestedTags = new HashSet<>(relations);
+            nestedTags.remove(tag); // remove current
+            String compositeIdKey = getCompositeIdKey(principal, tag, path);
+            String id = getKeyHash(compositeIdKey);
+            Acl parsedTag = Utils.parseTag(tag);
+            if (parsedTag == null) {
+                continue;
+            }
+
+            cache.add(RelCache.builder()
+                                .id(id)
+                                .rev(rev)
+                                .nsobject(Utils.createNsObject(parsedTag.getNamespace(), parsedTag.getObject()))
+                                .relation(parsedTag.getRelation())
+                                .nestedRelations(nestedTags)
+                                .usr(principal)
+                                .path(path)
+                                .build());
+        }
+
+        relCacheRepository.saveAll(cache);
+    }
+
+    private String getKeyHash(String compositeIdKey) {
+        long[] vec = MurmurHash3.hash128(compositeIdKey.getBytes(StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        for (long l : vec) {
+            sb.append(Long.toHexString(l));
+        }
+        return sb.toString();
+    }
+
+    protected String getCompositeIdKey(String principal, String tag, String path) {
+        return String.format("%s+%s+%s", principal, tag, path);
+    }
 }
--- auth/src/main/java/org/example/authserver/service/zanzibar/AclFilterService.java
+++ auth/src/main/java/org/example/authserver/service/zanzibar/AclFilterService.java
@@ -2,9 +2,13 @@ package org.example.authserver.service.zanzibar;
 
 import io.envoyproxy.envoy.service.auth.v3.CheckRequest;
 import io.jsonwebtoken.Claims;
+
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.authserver.entity.CheckResult;
 import org.example.authserver.service.CacheService;
@@ -12,115 +16,116 @@ import org.example.authserver.service.RelationsService;
 import org.example.authserver.service.model.LocalCache;
 import org.example.authserver.service.model.Mapping;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StopWatch;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class AclFilterService {
+    private final MappingService mappingService;
+    private final TokenService tokenService;
+    private final RelationsService relationsService;
+    private final CacheService cacheService;
 
-  private final MappingService mappingService;
-  private final TokenService tokenService;
-  private final RelationsService relationsService;
-  private final CacheService cacheService;
+    private final static StopWatch stopWatch = new StopWatch("AclFilter");
 
-  public AclFilterService(
-      RelationsService relationsService,
-      MappingService mappingService,
-      TokenService tokenService,
-      CacheService cacheService) {
-    this.mappingService = mappingService;
-    this.tokenService = tokenService;
-    this.relationsService = relationsService;
-    this.cacheService = cacheService;
-  }
+    public CheckResult checkRequest(CheckRequest request) {
+        stopWatch.start("getAllClaimsFromRequest");
+        Claims claims = tokenService.getAllClaimsFromRequest(request);
+        stopWatch.stop();
 
-  public CheckResult checkRequest(CheckRequest request) {
-    long start = System.currentTimeMillis();
-    Claims claims = tokenService.getAllClaimsFromRequest(request);
-    long time2 = System.currentTimeMillis();
-
-    if (claims == null) return CheckResult.builder().jwtPresent(false).result(false).build();
-
-    String user = claims.getSubject();
-    List<Mapping> mappings = mappingService.processRequest(request, claims);
-    long time3 = System.currentTimeMillis();
-    if (mappings == null || mappings.size() == 0) {
-      log.debug("Unable to find mapping for user {}.", user);
-      return CheckResult.builder().mappingsPresent(false).result(false).build();
-    }
-
-    LocalCache localCache = new LocalCache();
-    Long maxAclUpdate = relationsService.getAclMaxUpdate(user);
-
-    Set<String> allowedTags = new HashSet<>();
-    for (Mapping mapping : mappings) {
-      String mappingId = mapping.getVariable("aclId");
-      String namespace = mapping.getVariable("namespace");
-      String object = mapping.getVariable("object");
-      String path = mapping.getMappingEntity().getPath();
-
-      Set<String> roles = new HashSet<>(mapping.getRoles());
-      if (roles.isEmpty()) {
-        return CheckResult.builder()
-            .mappingsPresent(true)
-            .rejectedWithMappingId(mappingId)
-            .result(false)
-            .build();
-      }
-
-      long time4 = System.currentTimeMillis();
-      Set<String> relations = new HashSet<>();
-      // cacheService.getCachedRelations(user, namespace, object, path, maxAclUpdate);
-      boolean r = false;
-      if (HasTag(relations, roles, namespace, object)) {
-        r = true;
-        allowedTags.addAll(relations);
-        log.info("zanzibar.getRelations (cache) {} ms.", System.currentTimeMillis() - time4);
-      } else { // no actual cache exists
-        relations = relationsService.getRelations(namespace, object, user, localCache);
-        log.info("zanzibar.getRelations {} ms.", System.currentTimeMillis() - time4);
-
-        if (HasTag(relations, roles, namespace, object)) {
-          r = true;
-          allowedTags.addAll(relations);
+        if (Objects.isNull(claims)) {
+            return CheckResult.builder().jwtPresent(false).result(false).build();
         }
-        // cacheService.persistCacheAsync(user, relations, path, maxAclUpdate);
-      }
 
-      if (!r) {
-        log.info("expected roles: {}:{} {}", namespace, object, roles);
-        log.info("roles available for {}: {}", user, relations);
-        long end = System.currentTimeMillis();
-        log.info("checkRequest {} ms.", end - start);
+        stopWatch.start("mappingService.processRequest");
+        String user = claims.getSubject();
+        List<Mapping> mappings = mappingService.processRequest(request, claims);
+        stopWatch.stop();
+
+        if (CollectionUtils.isEmpty(mappings)) {
+            log.debug("Unable to find mapping for user {}.", user);
+            return CheckResult.builder().mappingsPresent(false).result(false).build();
+        }
+
+        stopWatch.start("relationsService.getAclMaxUpdate");
+        Long maxAclUpdate = relationsService.getAclMaxUpdate(user); // found revision
+        stopWatch.stop();
+
+        Set<String> allowedTags = new HashSet<>();
+        for (Mapping mapping : mappings) {
+            String mappingId = mapping.getVariable("aclId");
+            String namespace = mapping.getVariable("namespace");
+            String object = mapping.getVariable("object");
+            String path = mapping.getMappingEntity().getPath();
+
+            Set<String> roles = new HashSet<>(mapping.getRoles());
+            if (roles.isEmpty()) {
+                return CheckResult.builder()
+                        .mappingsPresent(true)
+                        .rejectedWithMappingId(mappingId)
+                        .result(false)
+                        .build();
+            }
+
+            stopWatch.start("zanzibar.getRelations - cache for " + mappingId);
+            Set<String> relations = cacheService.getCachedRelations(user, namespace, object, path, maxAclUpdate); // get from cache
+            stopWatch.stop();
+
+            if (checkTag(relations, roles, namespace, object)) { //cache hit
+                allowedTags.addAll(relations);
+            } else {                                             // cache miss
+                stopWatch.start("zanzibar.getRelations - for " + mappingId);
+                relations = relationsService.getRelations(namespace, object, user, new LocalCache());
+
+                if (checkTag(relations, roles, namespace, object)) {
+                    allowedTags.addAll(relations);
+                    cacheService.persistCacheAsync(user, relations, path, maxAclUpdate); // cache warming
+                    stopWatch.stop();
+                } else {
+                    log.info("expected roles: {}:{} {}", namespace, object, roles);
+                    log.info("roles available for {}: {}", user, relations);
+                    stopWatch.stop();
+                    log.info(timingsPrettyPrint());
+                    return CheckResult.builder()
+                            .mappingsPresent(true)
+                            .rejectedWithMappingId(mappingId)
+                            .result(false)
+                            .build();
+                }
+            }
+        }
+
+        log.info("mappings size: {}.", mappings.size());
+        log.info(timingsPrettyPrint());
         return CheckResult.builder()
-            .mappingsPresent(true)
-            .rejectedWithMappingId(mappingId)
-            .result(false)
-            .build();
-      }
+                .mappingsPresent(true)
+                .result(true)
+                .tags(allowedTags)
+                .build();
     }
 
-    long end = System.currentTimeMillis();
-    log.info("getAllClaimsFromRequest {} ms.", time2 - start);
-    log.info("mappingService.processRequest {} ms.", time3 - time2);
-    log.info("checkRequest {} ms.", end - start);
-    log.info("mappings size: {}.", mappings.size());
-    return CheckResult.builder().mappingsPresent(true).result(true).tags(allowedTags).build();
-  }
+    private static boolean checkTag(Set<String> relations, Set<String> roles, String namespace, String object) {
+        if (CollectionUtils.isEmpty(relations)) {
+            return false;
+        }
 
-  private static boolean HasTag(
-      Set<String> relations, Set<String> roles, String namespace, String object) {
-    if (relations == null || relations.isEmpty()) {
-      return false;
+        for (String role : roles) {
+            String currentTag = String.format("%s:%s#%s", namespace, object, role);
+            boolean tagFound = relations.contains(currentTag);
+            if (tagFound) {
+                log.trace("Found tag: {}", currentTag);
+                return true;
+            }
+        }
+        return false;
     }
 
-    for (String role : roles) {
-      String currentTag = String.format("%s:%s#%s", namespace, object, role);
-      boolean tagFound = relations.contains(currentTag);
-      if (tagFound) {
-        log.trace("Found tag: {}", currentTag);
-        return true;
-      }
+    private static String timingsPrettyPrint() {
+        return System.getProperty("line.separator")
+                + "==============================" + System.getProperty("line.separator")
+                + stopWatch.prettyPrint() + System.getProperty("line.separator") +
+               "==============================";
     }
-    return false;
-  }
 }
