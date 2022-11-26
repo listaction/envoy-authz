3b5179a0e891d2b0131b6892dfdbf1d322d6e080
Alex Kuleshov <alex@kuleshov.tech>
2022-11-25 20:02:50 -0400

    added debug query methods, refactoring

--- acl/src/main/java/authserver/common/CheckTestDto.java
+++ acl/src/main/java/authserver/common/CheckTestDto.java
@@ -1,5 +1,8 @@
 package authserver.common;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,4 +17,5 @@ public class CheckTestDto {
   private CheckRequestDTO request;
   private boolean result;
   private Map<String, String> resultHeaders;
+  private ZonedDateTime time = ZonedDateTime.ofInstant(Instant.now(), ZoneId.of("UTC"));
 }
