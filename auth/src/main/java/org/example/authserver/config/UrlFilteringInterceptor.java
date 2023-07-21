5aaa0e564cfbb5b96374403249aa951db4f37cb5
george.ovechin <no@mail>
2023-07-21 09:16:19 -0300

    URL filtering, double slashes are prohibited

---
+++ auth/src/main/java/org/example/authserver/config/UrlFilteringInterceptor.java
@@ -0,0 +1,24 @@
+package org.example.authserver.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class UrlFilteringInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    StringBuffer url = request.getRequestURL();
+    url.delete(0,7); // http://
+    if (url.indexOf("//") != -1) {
+      log.error("Double slash detected, blocking url {}", request.getRequestURL());
+      response.sendError(400, "Double slash in URL is prohibited");
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ auth/src/main/java/org/example/authserver/config/WebConfig.java
@@ -0,0 +1,13 @@
+package org.example.authserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UrlFilteringInterceptor());
+    }
+}
