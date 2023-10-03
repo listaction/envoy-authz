0792663430262468dbad8f6d97cbad0448f7368b
george-ovechkin <116825234+george-ovechkin@users.noreply.github.com>
2023-10-03 20:51:09 +0400

    full signout feature (#40)

--- auth/src/main/java/org/example/authserver/config/Constants.java
+++ auth/src/main/java/org/example/authserver/config/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
   public static final String ACL_REDIS_KEY = "acls";
   public static final String ACL_REL_CONFIG_REDIS_KEY = "rel_configs";
   public static final String SIGNOUT_REDIS_KEY = "SIGNOUT_%s";
+  public static final String USER_SIGNOUT_REDIS_KEY = "USER_SIGNOUT_%s";
   public static final String NEED_REFRESH_MAPPING_CACHE_MARKER_KEY =
       "NEED_REFRESH_MAPPING_CACHE_MARKER";
 }
--- auth/src/main/java/org/example/authserver/controller/SignoutController.java
+++ auth/src/main/java/org/example/authserver/controller/SignoutController.java
@@ -1,6 +1,8 @@
 package org.example.authserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.authserver.service.AuthService;
+import org.example.authserver.service.RedisService;
 import org.example.authserver.service.SignoutService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +13,30 @@ public class SignoutController {
 
   private final SignoutService signoutService;
 
-  public SignoutController(SignoutService signoutService) {
+  private final AuthService authService;
+
+  public SignoutController(
+      SignoutService signoutService, AuthService authService, RedisService redis) {
     this.signoutService = signoutService;
+    this.authService = authService;
   }
 
   @PostMapping("/{tenant}/{jti}/{expirationTime}")
   public void signout(
       @PathVariable String tenant, @PathVariable String jti, @PathVariable long expirationTime) {
+    log.debug("signout");
     signoutService.signout(tenant, jti, expirationTime);
+  }
+
+  @PostMapping("/{tenant}/{userId}")
+  public void userSignout(@PathVariable String tenant, @PathVariable String userId) {
+    log.debug("user signout");
+    signoutService.userSignout(tenant, userId);
+  }
+
+  @DeleteMapping("/{tenant}/{userId}")
+  public void removeUserSignoutKey(@PathVariable String tenant, @PathVariable String userId) {
+    log.debug("remove user signout key");
+    authService.removeUserSignoutKey(userId);
   }
 }
