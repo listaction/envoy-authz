e80c3f15254eea06714f46ecba65c6531a6df2e8
george.ovechin <no@mail>
2023-10-03 13:40:21 -0300

    full signout feature

--- auth/src/main/java/org/example/authserver/controller/SignoutController.java
+++ auth/src/main/java/org/example/authserver/controller/SignoutController.java
@@ -1,6 +1,8 @@
 package org.example.authserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.authserver.service.AuthService;
+import org.example.authserver.service.RedisService;
 import org.example.authserver.service.SignoutService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +13,30 @@ public class SignoutController {
 
   private final SignoutService signoutService;
 
-  public SignoutController(SignoutService signoutService) {
+  private final AuthService authService;
+
+  public SignoutController(
+      SignoutService signoutService, AuthService authService, RedisService redis) {
     this.signoutService = signoutService;
+    this.authService = authService;
   }
 
   @PostMapping("/{tenant}/{jti}/{expirationTime}")
   public void signout(
       @PathVariable String tenant, @PathVariable String jti, @PathVariable long expirationTime) {
+    log.debug("signout");
     signoutService.signout(tenant, jti, expirationTime);
+  }
+
+  @PostMapping("/full/{tenant}/{userId}")
+  public void fullSignout(@PathVariable String tenant, @PathVariable String userId) {
+    log.debug("full signout");
+    signoutService.fullSignout(tenant, userId);
+  }
+
+  @DeleteMapping("/full/{tenant}/{userId}")
+  public void removeFullSignoutKey(@PathVariable String tenant, @PathVariable String userId) {
+    log.debug("remove full signout key");
+    authService.removeFullSignoutKey(userId);
   }
 }
--- auth/src/main/java/org/example/authserver/service/SignoutService.java
+++ auth/src/main/java/org/example/authserver/service/SignoutService.java
@@ -32,4 +32,13 @@ public class SignoutService {
 
     return diff > 0 ? Long.valueOf(diff).intValue() : 1;
   }
+
+  public void fullSignout(String tenant, String userId) {
+    try {
+      String key = String.format(Constants.FULL_SIGNOUT_REDIS_KEY, userId);
+      redisService.set(key, "1");
+    } catch (Exception exception) {
+      log.warn("Redis service is unavailable", exception);
+    }
+  }
 }
